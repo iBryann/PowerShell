8c4f087b2b54df00ca65129b7ec3577d232e2c8b
Bryann Henrique <bryann.enrique@gmail.com>
2022-06-15 16:57:13 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -4,6 +4,6 @@ clear-host;
 function prompt {
     $path = (get-location).path;
 
-    write-host $path -ForegroundColor DarkGreen;
+    write-host "`n$path" -ForegroundColor DarkGreen;
     "> ";
 }
