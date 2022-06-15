e61a859714ac82fc9f68a39a37b26a81833e8580
Bryann Henrique <bryann.enrique@gmail.com>
2022-06-15 16:48:42 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,9 @@
 # Basta colocar esse arquivo na pasta do PS no System32.
-# > explorer $PSHOME // Abre a pasta
 clear-host;
 
 function prompt {
     $path = (get-location).path;
 
     write-host $path -ForegroundColor DarkGreen;
-    return "> ";
+    "> ";
 }
